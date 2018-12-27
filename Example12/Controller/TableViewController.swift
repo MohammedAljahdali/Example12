efa8c3d1da5428f7d64a7cf7e374c0893fc5907d
Ammar Tahhan <amr.tahhan@gmail.com>
2018-12-27 16:44:40 +0300

    Add solution

--- Example12/Controller/TableViewController.swift
+++ Example12/Controller/TableViewController.swift
@@ -41,27 +41,7 @@ extension TableViewController: UITableViewDelegate, UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "studentCell", for: indexPath) as! StudentTableViewCell
-     
-        cell.studentLocation = locations[indexPath.row]
-     
-        return cell
+        return UITableViewCell()
      }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 70
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        let studentLocation = locations[indexPath.row]
-        guard let url = URL(string: studentLocation.mediaURL ?? ""), UIApplication.shared.canOpenURL(url) else {
-            self.showAlert(title: "Error", message: "Invalid url")
-            return
-        }
-        
-        UIApplication.shared.open(url, options: [:], completionHandler: nil)
-    }
     
 }
